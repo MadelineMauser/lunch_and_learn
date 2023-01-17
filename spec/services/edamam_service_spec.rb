6c9028fc7ce149f7923a03fe93b6a7dafa969d4d
Madeline Mauser <madeline.mauser@gmail.com>
2023-01-17 16:54:55 -0700

    Add edamam service spec

---
+++ spec/services/edamam_service_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+RSpec.describe "Edamam Service", type: :request do
+  describe '.get_recipes' do
+    it 'returns recipe results for the inputted country with the needed attributes' do
+      response = EdamamService.get_recipes(france)
+
+      expect(response).to be_a(Hash)
+      expect(response[:hits]).to be_an(Array)
+      expect(response[:hits].first).to be_a(Hash)
+      expect(response[:hits].first[:recipe]).to be_a(Hash)
+      expect(response[:hits].first[:recipe][:uri]).to be_a(String)
+      expect(response[:hits].first[:recipe][:label]).to be_a(String)
+      expect(response[:hits].first[:recipe][:image]).to be_a(String)
+    end
+  end
+end
