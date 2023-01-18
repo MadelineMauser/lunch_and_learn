0630929977728ca75bf9cd29f471949e50bfc241
Madeline Mauser <madeline.mauser@gmail.com>
2023-01-17 22:51:19 -0700

    Add learning resource facade

---
+++ spec/facades/learning_resource_facade_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+RSpec.describe LearningResourceFacade do
+  it '.learning_resource', :vcr do
+    learning_resource = LearningResourceFacade.learning_resource("france")
+
+    expect(learning_resource).to be_a(LearningResource)
+    expect(learning_resource.country).to be_a(String)
+    expect(learning_resource.video).to be_a(Hash)
+    expect(learning_resource.video[:title]).to be_a(String)
+    expect(learning_resource.video[:youtube_video_id]).to be_a(String)
+    expect(learning_resource.images).to be_an(Array)
+    expect(learning_resource.images.first).to be_a(Hash)
+    expect(learning_resource.images.first[:alt_tag]).to be_a(String)
+    expect(learning_resource.images.first[:url]).to be_a(String)
+  end
+end
