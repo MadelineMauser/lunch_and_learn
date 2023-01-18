f949f452d7f0c8b49c283088b1db9d46de70d286
Madeline Mauser <madeline.mauser@gmail.com>
2023-01-17 17:39:14 -0700

    Add countries service spec

---
+++ spec/services/countries_service_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+RSpec.describe CountriesService do
+  describe '.get_countries' do
+    it 'returns country results with the needed attributes' do
+      response = CountriesService.get_countries
+
+      expect(response).to be_an(Array)
+      expect(response.first).to be_a(Hash)
+      expect(response.first[:name]).to be_a(Hash)
+      expect(response.first[:name][:common]).to be_a(String)
+    end
+  end
+end
