6e470a76fb9b5fc6a233f1674c985bf02b83f465
Madeline Mauser <madeline.mauser@gmail.com>
2023-01-17 22:28:40 -0700

    Add pexels service spec

---
+++ spec/services/pexels_service_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+RSpec.describe PexelsService do
+  describe '.get_images' do
+    it 'returns image results for the inputted country with the needed attributes', :vcr do
+      response = PexelsService.get_images("france")
+
+      expect(response).to be_a(Hash)
+      expect(response[:photos]).to be_an(Array)
+      expect(response[:items].first).to be_a(Hash)
+      expect(response[:items].first[:src]).to be_a(Hash)
+      expect(response[:items].first[:src][:original]).to be_a(String)
+      expect(response[:items].first[:alt]).to be_a(String)
+    end
+  end
+end
--- spec/services/youtube_service_spec.rb
+++ spec/services/youtube_service_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe YoutubeService do
   describe '.get_video' do
     it 'returns video results for the inputted country with the needed attributes', :vcr do
-      response = EdamamService.get_recipes("france")
+      response = YoutubeService.get_video("france")
 
       expect(response).to be_a(Hash)
       expect(response[:items]).to be_an(Array)
