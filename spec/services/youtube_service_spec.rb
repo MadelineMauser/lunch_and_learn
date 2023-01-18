43c8d975970427e3e9c2431437c0efd1d864cc1f
Madeline Mauser <madeline.mauser@gmail.com>
2023-01-17 22:20:00 -0700

    Add youtube service spec

---
+++ spec/services/youtube_service_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+RSpec.describe YoutubeService do
+  describe '.get_video' do
+    it 'returns video results for the inputted country with the needed attributes', :vcr do
+      response = EdamamService.get_recipes("france")
+
+      expect(response).to be_a(Hash)
+      expect(response[:items]).to be_an(Array)
+      expect(response[:items].first).to be_a(Hash)
+      expect(response[:items].first[:id]).to be_a(Hash)
+      expect(response[:items].first[:id][:videoId]).to be_a(String)
+      expect(response[:items].first[:snippet]).to be_a(Hash)
+      expect(response[:items].first[:snippet][:title]).to be_a(String)
+    end
+  end
+end
