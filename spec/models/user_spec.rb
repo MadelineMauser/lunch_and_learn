5a73bb72677947fe41e95ee7a7260ebcb7b3d94d
Madeline Mauser <madeline.mauser@gmail.com>
2023-01-18 02:30:09 -0700

    Add user model spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,8 +2,10 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   describe 'validations' do
-    it 'validates the presence of name, email, and api key' do
-      
-    end
+    it { should validate_presence_of(:name) }
+    it { should validate_presence_of(:email) }
+    it { should validate_presence_of(:api_key) }
+    it { should validate_uniqueness_of(:email) }
+    it { should validate_uniqueness_of(:api_key) }
   end
 end
