9a31271ca0c0bddc07315e5515380194eb2f4fef
Madeline Mauser <madeline.mauser@gmail.com>
2023-01-18 00:05:11 -0700

    Add pexels service

--- app/services/pexels_service.rb
+++ app/services/pexels_service.rb
@@ -1,13 +1,15 @@
 class PexelsService
   def self.conn 
-    Faraday.new(url: "https://api.edamam.com/api/recipes/v2")
+    Faraday.new(url: "https://api.pexels.com/v1/") do |f|
+      f.headers["Authorization"] = ENV['pexels_api_key']
+    end
   end
 
   def self.get_url(url)
     JSON.parse(conn.get(url).body, symbolize_names: true)
   end
   
-  def self.get_recipes(country)
-    get_url("?app_key=#{ENV["edamam_api_key"]}&app_id=#{ENV["edamam_app_id"]}&q=#{country}&type=public")
+  def self.get_images(country)
+    get_url("search?query=#{country}&per_page=10")
   end
 end
--- spec/services/pexels_service_spec.rb
+++ spec/services/pexels_service_spec.rb
@@ -4,13 +4,13 @@ RSpec.describe PexelsService do
   describe '.get_images' do
     it 'returns image results for the inputted country with the needed attributes', :vcr do
       response = PexelsService.get_images("france")
-
+      
       expect(response).to be_a(Hash)
       expect(response[:photos]).to be_an(Array)
-      expect(response[:items].first).to be_a(Hash)
-      expect(response[:items].first[:src]).to be_a(Hash)
-      expect(response[:items].first[:src][:original]).to be_a(String)
-      expect(response[:items].first[:alt]).to be_a(String)
+      expect(response[:photos].first).to be_a(Hash)
+      expect(response[:photos].first[:src]).to be_a(Hash)
+      expect(response[:photos].first[:src][:original]).to be_a(String)
+      expect(response[:photos].first[:alt]).to be_a(String)
     end
   end
 end
